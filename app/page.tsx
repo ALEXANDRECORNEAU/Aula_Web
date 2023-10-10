6f553d329f40e399a8258d5dfde6ccf294e8c9f0
ALEXANDRECORNEAU <106288769+ALEXANDRECORNEAU@users.noreply.github.com>
2023-10-10 23:47:51 +0000

    çç

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import  Banner  from "./components/Banner";
 import Header from "./components/Header";
+import Courses from "./components/Courses";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Banner />
+      <Courses />
      
     </div>
 
