f00af2d34261ebc6aab7d0715715229f8f1de385
ALEXANDRECORNEAU <106288769+ALEXANDRECORNEAU@users.noreply.github.com>
2023-10-10 23:56:39 +0000

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import  Banner  from "./components/Banner";
 import Header from "./components/Header";
 import Courses from "./components/Courses";
-
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div>
       <Header />
       <Banner />
       <Courses />
+      <Footer/>
      
     </div>
 
