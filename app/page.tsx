6ec3b15dea16945a844da82b87f807c7d3198a5d
ALEXANDRECORNEAU <106288769+ALEXANDRECORNEAU@users.noreply.github.com>
2023-10-10 23:46:42 +0000

    Coursos

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import  Banner  from "./components/Banner";
 import Header from "./components/Header";
-import Courses from "./components/Courses";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Banner />
-      <Course />
+     
     </div>
 
   )
