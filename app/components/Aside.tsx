35831eb6e73d5fd1e3f8b0de0d92c8e6c5edee05
ALEXANDRECORNEAU <106288769+ALEXANDRECORNEAU@users.noreply.github.com>
2023-11-29 22:42:44 +0000

    .

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Aside() {
     return (
         <div className="w-1/6">
-            <a href="/admin/course">Listar Curso</a> <br/>
+            <a href="/admin/course"></a>
+            <Button variant="ghost">Listar Curso</Button> <br />
             <a href="/admin/course/new">Cadastrar Curso</a>
 
         </div>
